c01dd7224fc7094fabd4c0ac8479e18b06d62d7d
carl <jiale.liu@coolkit.cn>
2022-07-19 20:16:20 +0800

    feat: 增加存读Token方法

--- src/cache/index.ts
+++ src/cache/index.ts
@@ -1,16 +1,20 @@
 // 本地存储配置
 import { LocalStorage } from "node-localstorage";
 
-const localStorage = new LocalStorage("./src/cache/temp");
+const localStorage = new LocalStorage("cache");
 
 export const storage = {
   set(key: string, value: any) {
-    const valueObj = this.get(key);
-    valueObj[Object.keys(value)[0]] = value;
+    let valueObj = this.get(key) || {};
+    if (valueObj[Object.keys(value)[0]]) {
+      valueObj[Object.keys(value)[0]] = value[Object.keys(value)[0]];
+    } else {
+      valueObj = value;
+    }
     localStorage.setItem(key, JSON.stringify(valueObj));
   },
   get(key: string) {
-    return JSON.parse(<string>localStorage.getItem(key)) || "";
+    return JSON.parse(<string>localStorage.getItem(key)) || {};
   },
   remove(key: string) {
     localStorage.removeItem(key);
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,48 +1,70 @@
 import { createHmac } from "crypto";
+import { storage } from "../cache/index.js";
+import dayjs from "dayjs";
 
 // 混合模式，将其他类的方法添加到 derivedCtor 类的原型上。
 export const applyMixins = (derivedCtor: any, constructors: any[]) => {
-    constructors.forEach((baseCtor) => {
-        // 获取baseCtor的属性名称，循环输出
-        Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
-            // 定义属性
-            Object.defineProperty(
-                derivedCtor.prototype,
-                name,
-                // 获取自己的属性描述符
-                Object.getOwnPropertyDescriptor(baseCtor.prototype, name) || Object.create(null)
-            )
-        })
-    })
-}
+  constructors.forEach((baseCtor) => {
+    // 获取baseCtor的属性名称，循环输出
+    Object.getOwnPropertyNames(baseCtor.prototype).forEach((name) => {
+      // 定义属性
+      Object.defineProperty(
+        derivedCtor.prototype,
+        name,
+        // 获取自己的属性描述符
+        Object.getOwnPropertyDescriptor(baseCtor.prototype, name) || Object.create(null)
+      );
+    });
+  });
+};
 // Random {size}-digit alphanumeric
-export const nonce =  (size: Number = 8): string => Math.random().toString(36).slice(-size);
+export const nonce = (size: Number = 8): string => Math.random().toString(36).slice(-size);
 
 export const fnParams2Url = (obj: any) => {
-    let aUrl = []
-    let fnAdd = function(key: string, value: string) {
-        return key + '=' + value
-    }
-    for (let k in obj) {
-        aUrl.push(fnAdd(k, obj[k]))
-    }
-    return encodeURIComponent(aUrl.join('&'))
-}
+  let aUrl = [];
+  let fnAdd = function (key: string, value: string) {
+    return key + "=" + value;
+  };
+  for (let k in obj) {
+    aUrl.push(fnAdd(k, obj[k]));
+  }
+  return encodeURIComponent(aUrl.join("&"));
+};
 
 // Hmac-Sha256 Sign
 export const sign = (msg: object, appSecret: string, isFormat: boolean = false): string => {
-    let buffer;
-    if (isFormat){
-        let txt: Array<string> = [];
-        Object.keys(msg).forEach((key:string)=>{
-            // Type-Expression：
-            // https://bobbyhadz.com/blog/typescript-element-implicitly-has-any-type-expression
-            // https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#type-assertions
-            txt.push(`${key}=${msg[key as keyof typeof msg]}`);
-        })
-        buffer = txt.join("&");
-    }else{
-        buffer = Buffer.from(JSON.stringify(msg), "utf-8");
+  let buffer;
+  if (isFormat) {
+    let txt: Array<string> = [];
+    Object.keys(msg).forEach((key: string) => {
+      // Type-Expression：
+      // https://bobbyhadz.com/blog/typescript-element-implicitly-has-any-type-expression
+      // https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#type-assertions
+      txt.push(`${key}=${msg[key as keyof typeof msg]}`);
+    });
+    buffer = txt.join("&");
+  } else {
+    buffer = Buffer.from(JSON.stringify(msg), "utf-8");
+  }
+  return createHmac("sha256", appSecret).update(buffer).digest("base64");
+};
+
+// 存储token
+export const saveToken = (res: any, account: string) => {
+  storage.set(res.data.region, {
+    [account]: {
+      at: res.data.at,
+      rt: res.data.rt,
+      createTime: dayjs().format()
     }
-    return createHmac("sha256", appSecret).update(buffer).digest("base64");
+  });
+};
+
+// 读取token
+export const getToken = (region: string, account: string) => {
+  const valueObj = storage.get(region);
+  if (valueObj[account]) {
+    return valueObj[account].at;
+  }
+  return "";
 };
