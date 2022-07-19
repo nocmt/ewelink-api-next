071efab9fadf429c42bd1eaff06b04921d7c8ffd
carl <jiale.liu@coolkit.cn>
2022-07-19 20:15:26 +0800

    feat: 增加请求前后日志记录配置项

--- src/Base.ts
+++ src/Base.ts
@@ -1,4 +1,27 @@
-import axios from "axios";
+import axios, { Method } from "axios";
+import log4js from "log4js";
+import { getToken } from "./utils/index.js";
+import { storage } from "./cache/index.js";
+
+log4js.configure({
+  appenders: {
+    // allLog: { type: "dateFile", filename: "./logs/t", pattern: "yyyy-MM-dd.log", alwaysIncludePattern: true },
+    // http请求日志  http请求日志需要app.use引用一下， 这样才会自动记录每次的请求信息
+    httpLog: {
+      type: "dateFile",
+      filename: `logs/default.log`,
+      pattern: "yyyy-MM-dd",
+      keepFileExt: true,
+      alwaysIncludePattern: true,
+      flags: "w" // a 是追加日志，w是删除之前的
+    }
+  },
+  categories: {
+    default: { appenders: ["httpLog"], level: "info" }
+  }
+});
+
+const logger = log4js.getLogger();
 
 declare module "axios" {
   interface AxiosResponse<T = any> {
@@ -7,7 +30,7 @@ declare module "axios" {
   }
 }
 
-export type eWeLinkBaseOptions = { appid: string; appSecret: string; region: string; outputLog?: string | boolean };
+export type eWeLinkBaseOptions = { appid: string; appSecret: string; region: string; requestRecord?: boolean };
 
 export class eWeLinkBase {
   // appid与appSecret，可通过 https://dev.ewelink.cc 注册获取
@@ -15,8 +38,9 @@ export class eWeLinkBase {
   appSecret?: string;
   region?: string;
   endpoint: string = "https://eu-apia.coolkit.cc";
-  outputLog?: string | boolean = false;
-  // 创建一个axios实例
+  requestRecord?: boolean = false;
+  token: string = "";
+  account: string = "";
   request = axios.create({
     baseURL: this.endpoint,
     timeout: 30000,
@@ -28,43 +52,65 @@ export class eWeLinkBase {
     if (!options) return;
     if (options.appid) this.appid = options.appid;
     if (options.appSecret) this.appSecret = options.appSecret;
-    if (options.outputLog) this.outputLog = options.outputLog;
+    if (options.requestRecord) this.requestRecord = options.requestRecord;
     if (options.region) {
       this.endpoint = `https://${options.region}-apia.coolkit.${["cn", "test"].includes(options.region) ? "cn" : "cc"}`;
       this.region = options.region;
       this.request.defaults.baseURL = this.endpoint;
     }
 
-    if (options.outputLog) {
-      // 判断是否为目录，后续加日志选项
-    }
-
     // 添加请求拦截器
-    // this.request.interceptors.request.use(function (config) {
-    //     // 在发送请求之前做些什么
-    //     // return config;
-    //     // config.headers = {
-    //     //     "X-CK-Appid": options.appid,
-    //     //     "X-CK-Nonce": nonce()
-    //     // }
-    //
-    // }, function (error) {
-    //     // 对请求错误做些什么
-    //     return Promise.reject(error);
-    // });
+    this.request.interceptors.request.use(
+      function (config) {
+        if (options.requestRecord) {
+          logger.info("Send request：", {
+            url: config.url,
+            method: config.method,
+            baseURL: config.baseURL,
+            headers: config.headers,
+            params: config?.params,
+            data: config?.data
+          });
+        }
+        return config;
+      },
+      function (error) {
+        // 对请求错误做些什么
+        return Promise.reject(error);
+      }
+    );
     // 添加响应拦截器
     this.request.interceptors.response.use(
       function (response) {
         // 对响应数据做点什么
-        // console.debug(response.config.headers)
-        return Object.assign(response.data, { status: response.status });
+        const res = Object.assign(response.data, { status: response.status });
+        if (options.requestRecord) {
+          logger.info("Response received：", JSON.stringify(res));
+        }
+        return res;
       },
       function (error) {
         // 对响应错误做点什么
+        if (options.requestRecord) {
+          logger.error("Incorrect response received：", error);
+        }
         return Promise.reject(error);
       }
     );
   }
+  syncLocalToken = (region: string, account: string) => {
+    this.region = region;
+    this.account = account;
+    this.token = getToken(region, account);
+    console.info(this.token);
+    let createTime;
+    try {
+      createTime = storage.get(region)[account]?.createTime;
+    } catch (error) {
+      createTime = null;
+    }
+    return createTime;
+  };
 
   setUrl = (region: string) => {
     this.endpoint = `https://${region}-apia.coolkit.${region === "cn" ? "cn" : "cc"}`;
