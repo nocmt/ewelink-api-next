1f422a7dd26f458498c193c76873f7f8cffdd73c
carl <jiale.liu@coolkit.cn>
2022-07-26 21:09:20 +0800

    fix: 修正DelRoom的错误

--- src/web/apis/home/delRoom.ts
+++ src/web/apis/home/delRoom.ts
@@ -1,7 +1,7 @@
 import { BaseWebAPI } from "../../WebAPI.js";
 
 export type baseInfo = {
-  roomid: string;
+  id: string;
 };
 
 export interface DelRoom extends BaseWebAPI {}
@@ -9,7 +9,7 @@ export interface DelRoom extends BaseWebAPI {}
 export class DelRoom {
   async delRoom(options: baseInfo) {
     const params = {
-      id: options.roomid
+      id: options.id
     };
     return await this.root.request.delete("/v2/family/room", {
       params,
