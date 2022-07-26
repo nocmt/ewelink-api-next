e385c187c6f00fff5d4997d0e473d84aa7cab16b
carl <jiale.liu@coolkit.cn>
2022-07-26 21:11:40 +0800

    feat: 家庭、设备相关方法混合导出

--- src/web/apis/device/index.ts
+++ src/web/apis/device/index.ts
@@ -1,11 +1,83 @@
 import { applyMixins } from "../../../utils/index.js";
 import { WebAPI } from "../../WebAPI.js";
 import { GetAllThings } from "../device/getAllThings.js";
+import { AddDevice } from "./addDevice.js";
+import { AddGroup } from "./addGroup.js";
+import { AddGSMDevice } from "./addGSMDevice.js";
+import { CoverGroupDeviceList } from "./coverGroupDeviceList.js";
+import { DelDevice } from "./delDevice.js";
+import { DelGroupDeviceList } from "./delGroupDeviceList.js";
+import { DelGroup } from "./delGroup.js";
+import { DelOperationHistory } from "./delOperationHistory.js";
+import { DelShare } from "./delShare.js";
+import { GetGroup } from "./getGroup.js";
+import { GetOperationHistory } from "./getOperationHistory.js";
+import { GetOTAInfo } from "./getOTAInfo.js";
+import { GetThings } from "./getThings.js";
+import { GetThingStatus } from "./getThingStatus.js";
+import { SetAllThingStatus } from "./setAllThingStatus.js";
+import { SetDeviceInfo } from "./setDeviceInfo.js";
+import { SetDeviceTags } from "./setDeviceTags.js";
+import { SetGroup } from "./setGroup.js";
+import { SetGroupDeviceList } from "./setGroupDeviceList.js";
+import { SetGroupStatus } from "./setGroupStatus.js";
+import { SetShare } from "./setShare.js";
+import { SetThingStatus } from "./setThingStatus.js";
+import { Share } from "./share.js";
 
 export class Device {
   constructor(protected readonly root: WebAPI) {}
 }
 
-export interface Device extends GetAllThings {}
+export interface Device
+  extends GetAllThings,
+    AddDevice,
+    AddGroup,
+    AddGSMDevice,
+    CoverGroupDeviceList,
+    DelDevice,
+    DelGroup,
+    DelGroupDeviceList,
+    DelOperationHistory,
+    DelShare,
+    GetGroup,
+    GetOperationHistory,
+    GetOTAInfo,
+    GetThings,
+    GetThingStatus,
+    SetAllThingStatus,
+    SetDeviceInfo,
+    SetDeviceTags,
+    SetGroup,
+    SetGroupDeviceList,
+    SetGroupStatus,
+    SetShare,
+    SetThingStatus,
+    Share {}
 
-applyMixins(Device, [GetAllThings]);
+applyMixins(Device, [
+  GetAllThings,
+  AddDevice,
+  AddGroup,
+  AddGSMDevice,
+  CoverGroupDeviceList,
+  DelDevice,
+  DelGroup,
+  DelGroupDeviceList,
+  DelOperationHistory,
+  DelShare,
+  GetGroup,
+  GetOperationHistory,
+  GetOTAInfo,
+  GetThings,
+  GetThingStatus,
+  SetAllThingStatus,
+  SetDeviceInfo,
+  SetDeviceTags,
+  SetGroup,
+  SetGroupDeviceList,
+  SetGroupStatus,
+  SetShare,
+  SetThingStatus,
+  Share
+]);
--- src/web/apis/home/index.ts
+++ src/web/apis/home/index.ts
@@ -1,11 +1,47 @@
 import { applyMixins } from "../../../utils/index.js";
 import { WebAPI } from "../../WebAPI.js";
 import { HomePage } from "./homePage.js";
+import { AddFamily } from "./addFamily.js";
+import { AddRoom } from "./addRoom.js";
+import { DelFamily } from "./delFamily.js";
+import { DelRoom } from "./delRoom.js";
+import { GetFamily } from "./getFamily.js";
+import { SetFamily } from "./setFamily.js";
+import { SetRoom } from "./setRoom.js";
+import { SetThing } from "./setThing.js";
+import { SortRoom } from "./sortRoom.js";
+import { SortThing } from "./sortThing.js";
+import { SwitchFamily } from "./switchFamily.js";
 
 export class Home {
   constructor(protected readonly root: WebAPI) {}
 }
 
-export interface Home extends HomePage {}
+export interface Home
+  extends HomePage,
+    AddFamily,
+    AddRoom,
+    DelFamily,
+    DelRoom,
+    GetFamily,
+    SetFamily,
+    SetRoom,
+    SetThing,
+    SortRoom,
+    SortThing,
+    SwitchFamily {}
 
-applyMixins(Home, [HomePage]);
+applyMixins(Home, [
+  HomePage,
+  AddFamily,
+  AddRoom,
+  DelFamily,
+  DelRoom,
+  GetFamily,
+  SetFamily,
+  SetRoom,
+  SetThing,
+  SortRoom,
+  SortThing,
+  SwitchFamily
+]);
