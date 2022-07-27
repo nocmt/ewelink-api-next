05840aaf64fb8567bb78bea32a7eb6ba8caade1c
carl <jiale.liu@coolkit.cn>
2022-07-27 17:19:51 +0800

    fix: fix .gitignore

--- dist/test/testConfig.d.ts
+++
@@ -1,7 +0,0 @@
-import { WebAPI } from "../src/index.js";
-export declare const testConfig: {
-    appid: string;
-    appSecret: string;
-    region: string;
-};
-export declare const client: WebAPI;
