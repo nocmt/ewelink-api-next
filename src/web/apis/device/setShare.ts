e2b503ebf26f7aa7dadb414fda9e7dba7ab9e7e8
carl <jiale.liu@coolkit.cn>
2022-07-29 16:21:24 +0800

    fix: Modify the parameter deviceId of SetShare

--- src/web/apis/device/setShare.ts
+++ src/web/apis/device/setShare.ts
@@ -1,7 +1,7 @@
 import { BaseWebAPI } from "../../WebAPI.js";
 
 export type deviceInfo = {
-  deviceid: string;
+  deviceId: string;
   apiKey: string;
   permit: number | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 15;
 };
@@ -11,11 +11,11 @@ export interface SetShare extends BaseWebAPI {}
 export class SetShare {
   async setShare(options: deviceInfo) {
     const body = {
-      deviceid: options.deviceid,
+      deviceId: options.deviceId,
       apiKey: options.apiKey,
       permit: options.permit
     };
-    return await this.root.request.post("/v2/device/share", body, {
+    return await this.root.request.post("/v2/device/share/permit", body, {
       headers: {
         Authorization: `Bearer ${this.root.token}`
       }
