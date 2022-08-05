28a1c41a3d372f8e451701e5bb5e67c34f4473c0
carl <jiale.liu@coolkit.cn>
2022-08-05 10:02:29 +0800

    feat: Add the definition of log print object during instantiation

--- src/Base.ts
+++ src/Base.ts
@@ -3,26 +3,8 @@ import log4js from "log4js";
 import { getToken, nonce } from "./utils/index.js";
 import { storage } from "./cache/index.js";
 
-log4js.configure({
-  appenders: {
-    // allLog: { type: "dateFile", filename: "./logs/t", pattern: "yyyy-MM-dd.log", alwaysIncludePattern: true },
-    // http请求日志  http请求日志需要app.use引用一下， 这样才会自动记录每次的请求信息
-    // HTTP request log HTTP request log requires app Use to quote, so that each request information can be automatically recorded
-    httpLog: {
-      type: "dateFile",
-      filename: `logs/default.log`,
-      pattern: "yyyy-MM-dd",
-      keepFileExt: true,
-      alwaysIncludePattern: true,
-      flags: "a" // 'a' is to append log, 'w' is the one before deletion
-    }
-  },
-  categories: {
-    default: { appenders: ["httpLog"], level: "info" }
-  }
-});
-
-const logger = log4js.getLogger();
+// const logger = log4js.getLogger();
+let _logger: any;
 
 declare module "axios" {
   interface AxiosResponse<T = any> {
@@ -31,7 +13,14 @@ declare module "axios" {
   }
 }
 
-export type eWeLinkBaseOptions = { appId: string; appSecret: string; region: string; requestRecord?: boolean };
+export type eWeLinkBaseOptions = {
+  appId: string;
+  appSecret: string;
+  region: string;
+  requestRecord?: boolean;
+  logObj?: any;
+  logLevel?: string | "info" | "debug" | "warn" | "error" | "fatal";
+};
 
 export class eWeLinkBase {
   // appId与appSecret，可通过 https://dev.ewelink.cc 注册获取
@@ -48,11 +37,35 @@ export class eWeLinkBase {
     timeout: 30000
   });
   storage = storage;
+  logger: log4js.Logger | undefined;
+  logLevel?: string | "info" | "debug" | "warn" | "error" | "fatal";
 
   // constructor 是一种用于创建和初始化class创建的对象的特殊方法，类似于Python的__init__函数
   // constructor is a special method that is used to create and initialize class objects, similar to the Python __init__ function
   constructor(options?: eWeLinkBaseOptions) {
     if (!options) return;
+
+    this.logLevel = options.logLevel || "debug";
+    log4js.configure({
+      appenders: {
+        // allLog: { type: "dateFile", filename: "./logs/t", pattern: "yyyy-MM-dd.log", alwaysIncludePattern: true },
+        // http请求日志  http请求日志需要app.use引用一下， 这样才会自动记录每次的请求信息
+        // HTTP request log HTTP request log requires app Use to quote, so that each request information can be automatically recorded
+        httpLog: {
+          type: "dateFile",
+          filename: `logs/default.log`,
+          pattern: "yyyy-MM-dd",
+          keepFileExt: true,
+          alwaysIncludePattern: true,
+          flags: "a" // 'a' is to append log, 'w' is the one before deletion
+        }
+      },
+      categories: {
+        default: { appenders: ["httpLog"], level: this.logLevel }
+      }
+    });
+    this.logger = options.logObj || log4js.getLogger(options.appId);
+    _logger = this.logger;
     if (options.appId) this.appId = options.appId;
     if (options.appSecret) this.appSecret = options.appSecret;
     if (options.requestRecord) this.requestRecord = options.requestRecord;
@@ -65,12 +78,12 @@ export class eWeLinkBase {
     // 添加请求拦截器
     // Add a request interceptor
     this.request.interceptors.request.use(
-      function (config: AxiosRequestConfig) {
+      (config: AxiosRequestConfig) => {
         if (config.headers) {
           config.headers["X-CK-Nonce"] = nonce();
         }
         if (options.requestRecord) {
-          logger.info("Send request：", {
+          _logger.info("Send request：", {
             url: config.url,
             method: config.method,
             baseURL: config.baseURL,
@@ -78,6 +91,7 @@ export class eWeLinkBase {
             params: config?.params,
             data: JSON.stringify(config?.data)
           });
+          _logger.debug("123");
         }
         return config;
       },
@@ -95,7 +109,7 @@ export class eWeLinkBase {
         // Do something with response data
         const res = Object.assign({ status: response.status }, response.data);
         if (options.requestRecord) {
-          logger.info("Response received：", JSON.stringify(res));
+          _logger.info("Response received：", JSON.stringify(res));
         }
         return res;
       },
@@ -103,7 +117,7 @@ export class eWeLinkBase {
         // 对响应错误做点什么
         // Do something with response error
         if (options.requestRecord) {
-          logger.error("Incorrect response received：", error);
+          _logger.error("Incorrect response received：", error);
         }
         return Promise.reject(error);
       }
