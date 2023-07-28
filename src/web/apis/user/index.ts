72032be9caa706e8ffc9710d396f467262ae4e55
carl <jiale.liu@coolkit.cn>
2023-07-28 14:33:02 +0800

    fix: Error modifying getRegion

--- src/web/apis/user/index.ts
+++ src/web/apis/user/index.ts
@@ -1,6 +1,6 @@
 import { applyMixins } from "../../../utils/index.js";
 import { WebAPI } from "../../WebAPI.js";
-import { GetRegion } from "./GetRegion.js";
+import { GetRegion } from "./getRegion.js";
 import { Login } from "./login.js";
 import { Register } from "./register.js";
 import { SMSLogin } from "./smsLogin.js";
