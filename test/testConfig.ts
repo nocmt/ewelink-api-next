50cde600acfa36f7f7bf6e2bb03c6ebb47998c57
carl <jiale.liu@coolkit.cn>
2023-08-21 16:25:11 +0800

    feat: Complete the document

--- test/testConfig.ts
+++ test/testConfig.ts
@@ -1,15 +1,15 @@
 import eWeLink from "../src/index.js";
 
 const client = new eWeLink.WebAPI({
-  appId: "lzHjzT8NipCQpRSQbhNzZZoDuTAkcI1G",
-  appSecret: "q75T54ondJlB8yioUdzBdAGBDzeEeDvK",
+  appId: "",
+  appSecret: "",
   region: "us",
   logObj: eWeLink.createLogger("us")
 });
 
 const wsClient = new eWeLink.Ws({
-  appId: "lzHjzT8NipCQpRSQbhNzZZoDuTAkcI1G",
-  appSecret: "q75T54ondJlB8yioUdzBdAGBDzeEeDvK",
+  appId: "",
+  appSecret: "",
   region: "us"
 });
 
